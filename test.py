18cccfc3f1099425dc42db11d350d675335c17e6
tahereT <taheretalaei@gmail.com>
2022-05-19 09:10:20 +0430

    add metadata

--- test.py
+++ test.py
@@ -3,52 +3,12 @@ from util import utils
 from vina import Vina
 
 
-# batch = "2020_11_04_CPJUMP1"
-# experiment_df = (
-#     pd.read_csv('output/experiment-metadata.tsv', sep='\t')
-#     .query('Batch==@batch')
-#     .query('Density==100')
-#     .query('Antibiotics=="absent"')
-# )
-
-#metadata = pd.read_csv('../2021_Chandrasekaran_submitted/metadata/external_metadata/JUMP-Target-1_compound_metadata.tsv', sep='\t')
-
 orf = pd.read_csv('Data/orf_pairs.csv')
 crispr = pd.read_csv('Data/crispr_pairs.csv')
 
 sorted_orf = orf.sort_values(by=['correlation'], ascending=False)
 sorted_crispr = crispr.sort_values(by=['correlation'], ascending=False)
 # print('-----------------------------------------------------')
-def download_all_protein(genes):
-    protiens = {}
-    for g in genes:
-        print(g)
-        try:
-            i = utils.get_rcsb_id(g)
-            utils.download_pdb(i)
-            protiens[g] = i
-        except:
-            i = []
-        print(i)
-        print('-------------------------')
-    print(list(protiens.values()))
-    utils.prepare_receptor(list(protiens.values()))
-    return protiens
-
-def download_all_ligands(inchikeys):
-    zincids = {}
-    for c in inchikeys:
-        print(c)
-        try:
-            zinc = utils.get_zincid(c)
-            utils.download_with_zincid(zinc, 'sdf')
-            zincids[c] = zinc
-        except:
-            zinc = []
-        print(zinc)
-    utils.prepare_ligand(list(zincids.values()))
-    return zincids
-
 def generate_zincids_csv(metadata):
     metadata["ZINCid"] = None
     for i in range(len(metadata)):
@@ -73,57 +33,13 @@ def generate_protien_csv(genes):
             protein_df = protein_df.append({'gene': gene, 'rcsb_id': None}, ignore_index=True)
     protein_df.to_csv('Data/metadata/receptor_metadata.csv', index=False)
 
-def docking(receptor, ligand):
-    v = Vina(sf_name='vina')
-    v.set_receptor(f'Data/receptor/{receptor}.pdbqt')
-
-    v.set_ligand_from_file(f'Data/ligands/{ligand}.pdbqt')
-    v.compute_vina_maps(center=[0, 0, 0], box_size=[20, 20, 20])
-
-    # Score the current pose
-    energy = v.score()
-    # print('Score before minimization: %.3f (kcal/mol)' % energy[0])
-
-    # Minimized locally the current pose
-    energy_minimized = v.optimize()
-    # print('Score after minimization : %.3f (kcal/mol)' % energy_minimized[0])
-    # v.write_pose('1iep_ligand_minimized.pdbqt', overwrite=True)
-
-    # Dock the ligand
-    v.dock(exhaustiveness=32, n_poses=5)
-    # v.write_poses('1iep_ligand_vina_out.pdbqt', n_poses=5, overwrite=True)
-
 if __name__ == '__main__':
-    ### download all proteins as receptors
-    # print(list(set(genes))
-    # download_all_protein(set(genes))
-    # print('all prteins prepared')
-    ### download all braod samples as ligans
-    # inchikeys = [metadata[metadata["broad_sample"] == broad_sample]["InChIKey"].item() for broad_sample in sorted_crispr["broad_sample_modality_1"]]
-    # download_all_ligands(set(inchikeys))
-    # print('all ligands prepared')
-    ### process for docking
-    # metadata["ZINCid"] = None
 
     metadata = pd.read_csv('Data/metadata/zinc_metadata_compound.csv')
     receptor_meta = pd.read_csv('Data/metadata/receptor_metadata.csv')
 
     zincids = list(metadata["ZINCid"])
     proteins = list(receptor_meta["rcsb_id"])
-
-    # for z in set(zincids):
-    #     z = str(z)
-    #     if z=='nan':
-    #         continue
-    #     utils.download_with_zincid(z , 'sdf')
-    # utils.prepare_ligand(zincids)
-    # for p in set(proteins):
-    #     if p=='nan':
-    #         continue
-    #     utils.download_pdb(p)
-    # utils.prepare_receptor(proteins)
-    #
-    # print('all is dowloaded........')
 
     for i in range(len(sorted_crispr)):
         broad_sample = sorted_crispr.iloc[i]["broad_sample_modality_1"]
@@ -154,7 +70,7 @@ if __name__ == '__main__':
             print('### ' ,receptor_true , ligand)
             print('***********************************************************')
             try:
-                docking(receptor_true, ligand)
+                utils.docking(receptor_true, ligand)
             except:
                 pass
 
@@ -163,11 +79,10 @@ if __name__ == '__main__':
             print('### ' ,receptor_predicted, ligand)
             print('***********************************************************')
             try:
-                docking(receptor_predicted, ligand)
+                utils.docking(receptor_predicted, ligand)
             except:
                 pass
 
         print('##################################################################')
         print('##################################################################')
 
-# metadata.to_csv('metadata_with_zinc.csv', index=False)
--- util/utils.py
+++ util/utils.py
@@ -4,6 +4,7 @@ import requests
 import subprocess
 import json
 import os
+from vina import Vina
 
 
 def get_zincid(inchikey):
@@ -29,13 +30,15 @@ def get_zincid(inchikey):
             line = line.split('/')[-2]
             zinc_id = urllib.parse.unquote(line)
             zinc_ids.append(str(zinc_id))
+    if len(zinc_ids) == 0:
+        print('no match found')
     return zinc_ids[0]
 
 
 def get_rcsb_id(gene):
     ### create a query for search in rcsb database and return rcsb_id
     ### this query is based on gene name in homo sapiens
-    ### this function returns first output id for query
+    ### this function returns first output rcsb_id for query
     #################################################################
     query = {
         "query": {
@@ -75,37 +78,43 @@ def get_rcsb_id(gene):
     }
 
     myurl = "https://search.rcsb.org/rcsbsearch/v2/query?json="
+    try:
+        req = urllib.request.Request(myurl)
+        req.add_header('Content-Type', 'application/json; charset=utf-8')
+        jsondata = json.dumps(query)
+        jsondataasbytes = jsondata.encode('utf-8')  # needs to be bytes
+        req.add_header('Content-Length', len(jsondataasbytes))
+        response = urllib.request.urlopen(req, jsondataasbytes)
+        with urllib.request.urlopen(req) as f:
+            response = f.read()
+        my_json = response.decode('utf8').replace("'", '"')
+        data = json.loads(my_json)
+        return data["result_set"][0]['identifier'][0:4]
+    except:
+        print('no match found')
+        return []
 
-    req = urllib.request.Request(myurl)
-    req.add_header('Content-Type', 'application/json; charset=utf-8')
-    jsondata = json.dumps(query)
-    jsondataasbytes = jsondata.encode('utf-8')  # needs to be bytes
-    req.add_header('Content-Length', len(jsondataasbytes))
-    response = urllib.request.urlopen(req, jsondataasbytes)
-    with urllib.request.urlopen(req) as f:
-        response = f.read()
-    my_json = response.decode('utf8').replace("'", '"')
-    data = json.loads(my_json)
-    return data["result_set"][0]['identifier'][0:4]
-
-def download_with_zincid(zincid, fr):
+def download_with_zincid(zincid, fr='sdf'):
     ### download sample from zinc15
     ### zincid: str
     ### fr: str - format of sample that you want to downalod from zinc15
+    ###           default is "sdf"
     ####################################################################
     if os.path.isdir(f'Data/ligands/{zincid}.{fr}') == False:
         print('download ligand' , zincid)
         urllib.request.urlretrieve(f'https://zinc15.docking.org/substances/{zincid}.{fr}', f'Data/ligands/{zincid}.{fr}')
-        #print(f'download {zincid}.{fr} ligand')
 
 def download_pdb(rcsbid):
-    ### download pbd file of protein from rcsb database
+    ### download pbd file of protein from rcsb database with rcsb_id
     rcsbid = str(rcsbid)
     if os.path.isdir(f'Data/receptor/{rcsbid}.pdb') == False:
         print('download pdb receptor', rcsbid)
         urllib.request.urlretrieve(f'https://files.rcsb.org/download/{rcsbid}.pdb', f'Data/receptor/{rcsbid}.pdb')
 
 def prepare_receptor(pdb_list):
+    ### convert pdb to pdbqt
+    ### input: list of pdb
+    ### save outputs at Data/receptor/
     for pdb in pdb_list:
         if os.path.isdir(f"Data/receptor/{pdb}.pdbqt"):
             continue
@@ -114,6 +123,9 @@ def prepare_receptor(pdb_list):
         subprocess.run(command)
 
 def prepare_ligand(ligand_list):
+    ### convert sdf to pdbqt
+    ### input: list of liagands
+    ### save outputs at Data/ligands/
     for ligand in ligand_list:
         if os.path.isdir(f"Data/ligands/{ligand}.pdbqt"):
             continue
@@ -122,6 +134,7 @@ def prepare_ligand(ligand_list):
         subprocess.run(command)
 
 def download_all_protein(genes):
+    ### input list of genes, and download pdb file for each gene
     protiens = {}
     for g in genes:
         try:
@@ -135,6 +148,7 @@ def download_all_protein(genes):
     return protiens
 
 def download_all_ligands(inchikeys):
+    ### input list of inchikeys, and download sdf file for each inchikey
     zincids = {}
     for c in inchikeys:
         try:
@@ -145,3 +159,23 @@ def download_all_ligands(inchikeys):
             pass
     prepare_ligand(list(zincids.values()))
     return zincids
+
+def docking(receptor, ligand, exhaustiveness=32, n_poses=20, maps_center=[0,0,0], box_size=[20,20,20]):
+    v = Vina(sf_name='vina')
+    v.set_receptor(f'Data/receptor/{receptor}.pdbqt')
+
+    v.set_ligand_from_file(f'Data/ligands/{ligand}.pdbqt')
+    v.compute_vina_maps(center=maps_center, box_size=[20,20,20])
+
+    # Score the current pose
+    #energy = v.score()
+    # print('Score before minimization: %.3f (kcal/mol)' % energy[0])
+
+    # Minimized locally the current pose
+    #energy_minimized = v.optimize()
+    # print('Score after minimization : %.3f (kcal/mol)' % energy_minimized[0])
+    # v.write_pose('1iep_ligand_minimized.pdbqt', overwrite=True)
+
+    # Dock the ligand
+    v.dock(exhaustiveness=exhaustiveness, n_poses=n_poses)
+    v.write_poses('Data/Docks/1iep_ligand_vina_out.pdbqt', n_poses=n_poses, overwrite=True)
